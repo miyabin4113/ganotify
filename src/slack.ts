0a6031ec94cf46613d72f00222aed22f9aa91119
miyabin66 <freezemark5166@gmail.com>
2021-04-04 22:09:53 +0900

    lint

--- src/slack.ts
+++ src/slack.ts
@@ -1,14 +1,15 @@
-import * as github from '@actions/github';
-import * as core from '@actions/core';
-import { SectionBlock, MessageAttachment, MrkdwnElement } from '@slack/types';
+import * as github from '@actions/github'
+import * as core from '@actions/core'
+import {SectionBlock, MessageAttachment, MrkdwnElement} from '@slack/types'
 import {
-  IncomingWebhook, IncomingWebhookSendArguments,
+  IncomingWebhook,
+  IncomingWebhookSendArguments,
   IncomingWebhookResult
-} from '@slack/webhook';
+} from '@slack/webhook'
 
 export class Slack extends IncomingWebhook {
   // 0: failure, 1: success
-  static readonly color: string[] = ['#cb2431', '#2cbe4e'];
+  static readonly color: string[] = ['#cb2431', '#2cbe4e']
 
   constructor(
     url: string,
@@ -16,38 +17,41 @@ export class Slack extends IncomingWebhook {
     icon_emoji: string = 'github',
     channel: string = '#general'
   ) {
-    super(url, {username, icon_emoji, channel});
+    super(url, {username, icon_emoji, channel})
   }
 
   /**
    * Get slack blocks UI
    */
   protected get blocks(): SectionBlock {
-    const context = github.context;
-    const { sha, eventName, workflow, ref } = context;
-    const { owner, repo } = context.repo;
-    const repo_url: string = `https://github.com/${owner}/${repo}`;
-    const action_url: string = `${repo_url}/commit/${sha}/checks`;
+    const context = github.context
+    const {sha, eventName, workflow, ref} = context
+    const {owner, repo} = context.repo
+    const repo_url: string = `https://github.com/${owner}/${repo}`
+    const action_url: string = `${repo_url}/commit/${sha}/checks`
 
     const blocks: SectionBlock = {
       type: 'section',
       fields: [
-        { type: 'mrkdwn', text: `*repository*\n<${repo_url}|${owner}/${repo}>` },
-        { type: 'mrkdwn', text: `*ref*\n${ref}` },
-        { type: 'mrkdwn', text: `*event name*\n${eventName}` },
-        { type: 'mrkdwn', text: `*workflow*\n<${action_url}|${workflow}>` },
+        {type: 'mrkdwn', text: `*repository*\n<${repo_url}|${owner}/${repo}>`},
+        {type: 'mrkdwn', text: `*ref*\n${ref}`},
+        {type: 'mrkdwn', text: `*event name*\n${eventName}`},
+        {type: 'mrkdwn', text: `*workflow*\n<${action_url}|${workflow}>`}
       ]
     }
 
-    return blocks;
+    return blocks
   }
 
   /**
    * Generate slack payload
    */
-  protected generatePayload(status: any, text: string): IncomingWebhookSendArguments {
-    const text_for_slack: MrkdwnElement = { type: 'mrkdwn', text };
-    const blocks: SectionBlock = { ...this.blocks, text: text_for_slack };
+  protected generatePayload(
+    status: any,
+    text: string
+  ): IncomingWebhookSendArguments {
+    const text_for_slack: MrkdwnElement = {type: 'mrkdwn', text}
+    const blocks: SectionBlock = {...this.blocks, text: text_for_slack}
     const attachments: MessageAttachment = {
       color: Slack.color[status],
       blocks: [blocks]
@@ -56,24 +60,30 @@ export class Slack extends IncomingWebhook {
       attachments: [attachments]
     }
 
-    core.debug(`Generated payload for slack: ${JSON.stringify(payload)}`);
+    core.debug(`Generated payload for slack: ${JSON.stringify(payload)}`)
 
-    return payload;
+    return payload
   }
 
   /**
    * Notify information about github actions to Slack
    */
-  public async notify(status: any, text: string): Promise<IncomingWebhookResult> {
+  public async notify(
+    status: any,
+    text: string
+  ): Promise<IncomingWebhookResult> {
     try {
-      const payload: IncomingWebhookSendArguments = this.generatePayload(status, text);
-      const result = await this.send(payload);
+      const payload: IncomingWebhookSendArguments = this.generatePayload(
+        status,
+        text
+      )
+      const result = await this.send(payload)
 
-      core.debug('Sent message to Slack');
+      core.debug('Sent message to Slack')
 
-      return result;
+      return result
     } catch (err) {
-      throw err;
+      throw err
     }
   }
 }
